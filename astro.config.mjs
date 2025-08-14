077fbd6b52db5902ffbf3dc47bcee893cf4a6d57
minhvu <minhvu@192.168.1.5>
2025-08-14 10:38:39 +0700

    fix: refactor layout for tailwind structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [react()]
-});
-import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
 });
